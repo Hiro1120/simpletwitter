46576b502ba6fecd0e2755095c35b45ac6b44e63
中山洸 <nakayama.hiroshi@alhinc.jp>
2022-03-25 18:15:34 +0900

    ユーザーのアカウント名の部分にリンクを設定し、クリックすると各ユーザー毎のつぶやき表示画面へ遷移する機能の実装

--- src/chapter6/dao/UserMessageDao.java
+++ src/chapter6/dao/UserMessageDao.java
@@ -30,7 +30,7 @@ public class UserMessageDao {
 			sql.append("INNER JOIN users ");
 			sql.append("ON messages.user_id = users.id ");
 			if (idNum != null) {
-				sql.append("WHERE id = ? ");
+				sql.append("WHERE messages.id = ? ");
 			}
 			sql.append("ORDER BY created_date DESC limit " + num);
 
